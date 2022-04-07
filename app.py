fb12e09cab5dbeb635fa1ade16e18c28e08f284f
Dennis Olsson <dennis@dluco.se>
2022-04-07 15:56:02 +0200

    refactor calculations to own module and output json. Prepare for api

--- app.py
+++ app.py
@@ -1,82 +1,8 @@
-import pandas as pd
 
-DESIRED_ACCOUNT = 'Rocket 🚀'
-FILE = 'data/transaktioner_2022-01-01_2022-03-26.csv'
+from pprint import pprint as pp
 
-pd.set_option('display.max_rows', None)
+from calculate import main as calculate
 
-# Read in the CSV file
-df = pd.read_csv(FILE, delimiter=";")
+calc = calculate('Rocket 🚀', 'data/transaktioner_2022-01-01_2022-03-26.csv')
 
-# Filter the dataframe to only include the desired account
-df = df[df['Konto'] == DESIRED_ACCOUNT]
-
-# Filter the datafram to only include ones that have ISIN not equal to -
-df = df[df['ISIN'] != '-']
-
-# Filter the dataframe to only include ones of the desired Typ av transaktion (Köp, Sälj)
-df = df[df['Typ av transaktion'].isin(['Köp', 'Sälj'])]
-
-# Convert Antal to int
-df['Antal'] = df['Antal'].astype(int)
-
-# Convert Belopp to float
-df['Belopp'] = df['Belopp'].astype(float)
-
-# Filter the datafram to only include transactions of same ISIN that have the same Antal
-df = df.groupby(['ISIN', 'Värdepapper/beskrivning']
-                ).filter(lambda x: x['Antal'].sum() == 0)
-
-group = df.groupby(['ISIN', 'Värdepapper/beskrivning'])
-
-sum_sell = group.apply(lambda x: x[x['Belopp'].gt(0)]['Belopp'].sum())
-sum_buy = group.apply(lambda x: x[x['Belopp'].lt(0)]['Belopp'].sum())
-
-# Calculate gain or loss
-sum_gain = sum_sell + sum_buy
-
-# Calculate the percentage change between the buy and sell
-percentage_change = ((sum_sell / sum_buy * -1) - 1) * 100
-
-# Create a dataframe with the desired columns
-output = pd.concat(
-    [
-        sum_sell.apply('{:.0f}'.format),
-        sum_buy.apply('{:.0f}'.format),
-        sum_gain.apply('{:.0f}'.format),
-        percentage_change.apply('{:,.2f} %'.format),
-    ],
-    axis=1,
-    keys=['Sell (SEK)', 'Buy (SEK)', 'Gain (SEK)', 'Change (%)'])
-
-# Sort the dataframe by the gain
-output = output.iloc[pd.to_numeric(output['Gain (SEK)']).argsort()[::-1]]
-
-profit = sum_gain.sum()
-
-
-print("### SUMMARY ###\n")
-
-print(f"Account:\t {DESIRED_ACCOUNT}")
-print(f"Period:\t\t {df['Datum'].min()} - {df['Datum'].max()}\n")
-
-print(f"Max gain (%):\t {percentage_change.max():.2f} % (* INSERT NAME *)")
-print(f"Max loss (%):\t {percentage_change.min():.2f} % (* INSERT NAME *)\n")
-
-print(f"Max gain (SEK):\t {sum_gain.max():.0f} SEK (* INSERT NAME *)")
-print(f"Max loss (SEK):\t {sum_gain.min():.0f} SEK (* INSERT NAME *)\n")
-
-print(
-    f"Average gain:\t {percentage_change[percentage_change > 0].mean():.2f} %")
-print(
-    f"Average loss:\t {percentage_change[percentage_change < 0].mean():.2f} %\n")
-
-print(
-    f"No. gain/loss:\t {percentage_change[percentage_change > 0].count()}/{percentage_change[percentage_change < 0].count()}")
-
-# print sum of all gains
-print(f"Total gain/loss: {profit:,.0f} SEK {'🍾' if profit > 0 else '🤮'}")
-
-print("\n### RESULTS ###\n")
-
-print(output)
+pp(calc, indent=2)
---
+++ calculate.py
@@ -0,0 +1,67 @@
+
+import pandas as pd
+
+
+def main(desired_account: str, file_path: str):
+    pd.set_option('display.max_rows', None)
+
+    # Read in the CSV file
+    df = pd.read_csv(file_path, delimiter=";")
+
+    # Filter the dataframe to only include the desired account
+    df = df[df['Konto'] == desired_account]
+
+    # Filter the datafram to only include ones that have ISIN not equal to -
+    df = df[df['ISIN'] != '-']
+
+    # Filter the dataframe to only include ones of the desired Typ av transaktion (Köp, Sälj)
+    df = df[df['Typ av transaktion'].isin(['Köp', 'Sälj'])]
+
+    # Convert Antal to int
+    df['Antal'] = df['Antal'].astype(int)
+
+    # Convert Belopp to float
+    df['Belopp'] = df['Belopp'].astype(float)
+
+    # Filter the datafram to only include transactions of same ISIN that have the same Antal
+    df = df.groupby(['ISIN', 'Värdepapper/beskrivning']
+                    ).filter(lambda x: x['Antal'].sum() == 0)
+
+    df['Sum buy/sell amount (SEK)'] = df.groupby(['ISIN', 'Typ av transaktion'])[
+        'Belopp'].transform('sum')
+
+    df['Gain/loss (SEK)'] = df.groupby('ISIN')['Belopp'].transform('sum')
+
+    df['Gain/loss (%)'] = df['Gain/loss (SEK)'] / \
+        (df[df['Sum buy/sell amount (SEK)'] < 0]
+         ['Sum buy/sell amount (SEK)'] * -1) * 100
+
+    # Sort dataframe
+    df = df.sort_values(by=['Värdepapper/beskrivning',
+                        'Datum'], ascending=[True, True])
+
+    output = df.groupby(['ISIN', 'Värdepapper/beskrivning'], as_index=False)[
+        ['ISIN', 'Värdepapper/beskrivning', 'Gain/loss (SEK)', 'Gain/loss (%)']].median()
+
+    return {
+        'account': desired_account,
+        'period': f"{df['Datum'].min()} - {df['Datum'].max()}",
+        'profit': output['Gain/loss (SEK)'].sum(),
+        'no': {
+            'gain': output[output['Gain/loss (%)'] > 0]['Gain/loss (%)'].count(),
+            'loss': output[output['Gain/loss (%)'] < 0]['Gain/loss (%)'].count(),
+        },
+        'max_gain': {
+            'percentage': output['Gain/loss (%)'].max(),
+            'amount': output['Gain/loss (SEK)'].max(),
+        },
+        'min_gain': {
+            'percentage': output['Gain/loss (%)'].min(),
+            'amount': output['Gain/loss (SEK)'].min(),
+        },
+        'avg': {
+            'gain': output[output['Gain/loss (%)'] > 0]['Gain/loss (%)'].mean(),
+            'loss': output[output['Gain/loss (%)'] < 0]['Gain/loss (%)'].mean(),
+        },
+        'transactions': output.to_json(orient='records', lines=False, force_ascii=False)
+    }
