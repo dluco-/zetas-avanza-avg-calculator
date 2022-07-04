cb2dcabf1744a6b21a105ac98b67310eba738a47
Dennis Olsson <dennis@dluco.se>
2022-07-04 22:36:43 +0200

    Use mark minervini calculations

--- calculate.py
+++ calculate.py
@@ -1,9 +1,11 @@
 
 import pandas as pd
+from numpy import outer
 
 
 def main(desired_account: str, file_path: str):
     pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
 
     # Read in the CSV file
     df = pd.read_csv(file_path, delimiter=";", decimal=",")
@@ -23,6 +25,12 @@ def main(desired_account: str, file_path: str):
     # Convert Belopp to float
     df['Belopp'] = df['Belopp'].astype(float)
 
+    # Convert Datum to Date
+    df['Datum'] = pd.to_datetime(df['Datum'])
+
+    # Filter the dataframe to only containe dates in may
+    # df = df[df['Datum'].dt.month == 5]
+
     # Filter the datafram to only include transactions of same ISIN that have the same Antal
     df = df.groupby(['ISIN', 'Värdepapper/beskrivning']
                     ).filter(lambda x: x['Antal'].sum() == 0)
@@ -36,34 +44,26 @@ def main(desired_account: str, file_path: str):
         (df[df['Sum buy/sell amount (SEK)'] < 0]
          ['Sum buy/sell amount (SEK)'] * -1) * 100
 
+    df['Gain/loss days'] = (df.groupby(['ISIN'])['Datum'].transform('max') -
+                            df.groupby(['ISIN'])['Datum'].transform('min')).dt.days
+
     # Sort dataframe
     df = df.sort_values(by=['Värdepapper/beskrivning',
                         'Datum'], ascending=[True, True])
 
     output = df.groupby(['ISIN', 'Värdepapper/beskrivning'], as_index=False)[
-        ['ISIN', 'Värdepapper/beskrivning', 'Gain/loss (SEK)', 'Gain/loss (%)']].median()
+        ['ISIN', 'Värdepapper/beskrivning', 'Gain/loss (SEK)', 'Gain/loss (%)', 'Gain/loss days']].median()
 
     return {
         'account': desired_account,
         'period': f"{df['Datum'].min()} - {df['Datum'].max()}",
-        'profit': output['Gain/loss (SEK)'].sum(),
-        'numbers': {
-            'gain': output[output['Gain/loss (%)'] > 0]['Gain/loss (%)'].count(),
-            'loss': output[output['Gain/loss (%)'] < 0]['Gain/loss (%)'].count(),
-        },
-        'max': {
-            'gain': {
-                'percentage': output['Gain/loss (%)'].max(),
-                'amount': output['Gain/loss (SEK)'].max(),
-            },
-            'loss': {
-                'percentage': output['Gain/loss (%)'].min(),
-                'amount': output['Gain/loss (SEK)'].min(),
-            }
-        },
-        'avg': {
-            'gain': output[output['Gain/loss (%)'] > 0]['Gain/loss (%)'].mean(),
-            'loss': output[output['Gain/loss (%)'] < 0]['Gain/loss (%)'].mean(),
-        },
+        'avg_gain': output[output['Gain/loss (%)'] > 0]['Gain/loss (%)'].mean(),
+        'avg_loss': output[output['Gain/loss (%)'] < 0]['Gain/loss (%)'].mean(),
+        'win_percentage': output[output['Gain/loss (%)'] > 0]['Gain/loss (%)'].count() / output['ISIN'].count() * 100,
+        'total_trades': output['ISIN'].count(),
+        'lg_gain': output[output['Gain/loss (%)'] > 0]['Gain/loss (%)'].max(),
+        'lg_loss': output[output['Gain/loss (%)'] < 0]['Gain/loss (%)'].min(),
+        'avg_gain_days': output[output['Gain/loss (%)'] > 0]['Gain/loss days'].mean(),
+        'avg_loss_days': output[output['Gain/loss (%)'] < 0]['Gain/loss days'].mean(),
         # 'transactions': output.to_json(orient='records', lines=False, force_ascii=False)
     }
